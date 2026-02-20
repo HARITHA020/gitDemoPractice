a7df74f1d508f5693e4323debfa45ff5104b3fcb
haritha <haritharavichandran11@gmail.com>
2026-02-20 22:08:36 +0530

    the third new work

--- src/gitPractice/Git1.java
+++ src/gitPractice/Git1.java
@@ -5,7 +5,8 @@ public class Git1 {
 	public static void main(String[] args) {
 		System.out.println("the new work is started");
 		System.out.println("the work is on going");
-
+		
+		System.out.println("the third work is running");
 	}
 
 }
